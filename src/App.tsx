f9fe8573d3c52a84a9ae14639804a3a3618ca7a9
parisa <parisa.naeim@gmail.com>
2023-07-16 23:22:31 +1000

    add bootstrap and style item

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import Footer from "./components/Footer"
-import Header from "./components/Header"
-import './App.css'
-
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import "./App.css";
+import Item from "./components/Item";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
-
-
   return (
     <>
       <Header></Header>
+      <Item></Item>
       <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Item.tsx
@@ -0,0 +1,22 @@
+function Item() {
+  return (
+    <div className="card">
+      <div className="card-body">
+        <p>name</p>
+        <p>description</p>
+        <p>price</p>
+      </div>
+    </div>
+  );
+}
+export default Item;
+{
+  /* <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div> */
+}
